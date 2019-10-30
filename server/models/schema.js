5a683d26c2f633fefa956bf7cacdf396e5818f60
aeirea <eirea99@gmail.com>
2019-10-30 14:32:35 -0400

    Added required functionality to work with Mongoose

--- server/models/schema.js
+++ server/models/schema.js
@@ -1,11 +1,14 @@
-var profileSchema = new Schema({
+var mongoose = require('mongoose'),
+		Schema = mongoose.Schema;
+
+var examTileSchema = new Schema({
 	_id: { type: Number, required: true, unique: true },
 	class: { type: String, required: true},
 	exam_num: { type: Number, required: true},
 	start: { type: Date, required: true},
 	end: { type: Date, required: true},
 	online: { type: Boolean, required: true},
-	location: {
+	location: { //This would only be used if not-online
 		buidling: String,
 		floor: String,
 		description: String
@@ -20,3 +23,15 @@ var userSchema = new Schema({
 	exams: Number, //This is going to be an array of the _id that the student is enrolled in
 	email: { type: String, required: true, unique: true}
 });
+
+examTileSchema.pre('save', function(next) {
+  /* your code here */
+  var currDate = new Date();
+  this.updated_at = currDate;
+  if(!this.created_at)
+    this.created_at - currDate;
+  next();
+});
+
+var examTile = mongoose.model('examTile', examTileSchema);
+module.exports = examTile;
