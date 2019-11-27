2b530490e31179e66b6cb8ac681e55c7e0a9e577
deventura <d.ventura@ufl.edu>
2019-11-27 10:08:20 -0500

    pushing package.json files

--- server/config/express.js
+++ server/config/express.js
@@ -1,43 +1,55 @@
-const path = require('path'),
-    express = require('express'),
-    mongoose = require('mongoose'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    examRouter = require('../routes/exam.server.routes');
-    userRouter = require('../routes/user.server.routes');
-    reservationRouter = require('../routes/reservation.server.routes');
+const path = require("path"),
+  express = require("express"),
+  mongoose = require("mongoose"),
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  examRouter = require("../routes/exam.server.routes");
+userRouter = require("../routes/user.server.routes");
+reservationRouter = require("../routes/reservation.server.routes");
+
+//Authentication
+var session = require("express-session");
 
 module.exports.init = () => {
-    mongoose.connect(process.env.DB_URI || require('./config').db.uri, {
-        useNewUrlParser: true
+  mongoose.connect(process.env.DB_URI || require("./config").db.uri, {
+    useNewUrlParser: true
+  });
+  mongoose.set("useCreateIndex", true);
+  mongoose.set("useFindAndModify", false);
+  mongoose.set("useUnifiedTopology", true);
+
+  // initialize app
+  const app = express();
+
+  // enable request logging for development debugging
+  app.use(morgan("dev"));
+
+  // body parsing middleware
+  app.use(bodyParser.json());
+
+  // add a router
+  app.use("/api/exams", examRouter);
+  app.use("/api/users", userRouter);
+  app.use("/api/reservations", reservationRouter);
+
+  if (process.env.NODE_ENV === "production") {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, "../../client/build")));
+
+    // Handle React routing, return all requests to React app
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
     });
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('useUnifiedTopology', true);
+  }
 
-    // initialize app
-    const app = express();
+  app.use(
+    session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: true
+      //cookie: { secure: true }
+    })
+  );
 
-    // enable request logging for development debugging
-    app.use(morgan('dev'));
-
-    // body parsing middleware
-    app.use(bodyParser.json());
-
-    // add a router
-    app.use('/api/exams', examRouter);
-    app.use('/api/users', userRouter);
-    app.use('/api/reservations', reservationRouter);
-
-    if (process.env.NODE_ENV === 'production') {
-        // Serve any static files
-        app.use(express.static(path.join(__dirname, '../../client/build')));
-
-        // Handle React routing, return all requests to React app
-        app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-        });
-    }
-
-    return app
-}
+  return app;
+};
