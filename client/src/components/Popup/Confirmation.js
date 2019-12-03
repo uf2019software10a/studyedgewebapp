facc4eb63ef149383336976e11f75ce1c280d7a7
Sean Minchin <34931614+seanminchin@users.noreply.github.com>
2019-12-03 02:10:56 -0500

    reservation can be added via popup

--- client/src/components/Popup/Confirmation.js
+++ client/src/components/Popup/Confirmation.js
@@ -4,15 +4,76 @@ import {militaryToStandard, isOnline, getMonthName} from "../DateTimeUtil"
 import axios from 'axios'
 
 class Confirmation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null
+        }
+    }
+
+    getUser = async (email) => {
+        console.log(email);
+        let res = await axios.get(`http://localhost:3000/api/users/userEmail=${email}`);
+        const data = res.data;
+        return data[0];
+    };
+
+    getReservations = async (ID) => {
+        let res = await axios.get(`http://localhost:3000/api/reservations/user=${ID}`);
+        let data = res.data;
+        console.log(data);
+    };
+
     infoSubmitted = () => {
         const userEmailAddr = this.email.value;
-        const specificTopicsPara = this.comments.value;
+        let specificTopicsPara = this.comments.value;
+        const userName = this.name.value;
         //console.log('my email: ', userEmailAddr);
         //console.log('my comments: ', specificTopicsPara);
         this.props.emailUpdate(userEmailAddr);
         this.props.closePopup();
         // TODO: setup this boolean
-        true ? this.props.openReservationConfirmedPopup() : this.props.openReservationErrorPopup();
+
+
+        /*
+        // get the user from the database based on their email
+        axios.get(`http://localhost:3000/api/users/userEmail=${userEmailAddr}`)
+            .then(res => {
+                const data = res.data;
+                this.user = data[0];
+                console.log(this.user);
+            })*/
+
+        this.getUser(userEmailAddr).then((data) => {
+            console.log(data);
+            if(specificTopicsPara.length < 1) {
+                specificTopicsPara = "N/A";
+            }
+            const reservation = {
+                user_id: data._id,
+                exam_id: this.props.session._id,
+                topics: specificTopicsPara
+            };
+            console.log(reservation);
+            // create the reservation
+            axios.post('http://localhost:3000/api/reservations/', reservation)
+                .then((res) => {
+                    console.log(res);
+                })
+        })
+
+        /*
+        // get list of exams associated with this user
+        axios.get(`http://localhost:3000/api/reservations/user=${this.user._id}`)
+            .then(res => {
+                const data = res.data;
+                this.reservations = data;
+                console.log(data);
+            }).catch((error) => {
+                console.log(error);
+        })
+         */
+        //true ? this.props.openReservationConfirmedPopup() : this.props.openReservationErrorPopup();
     };
 
     render() {
