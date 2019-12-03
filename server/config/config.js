3cd08072f34cd7ed9374982b2f03f15409141489
sminchin <32098376+sminchin@users.noreply.github.com>
2019-12-02 23:39:10 -0500

    added config

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+//This file holds any configuration variables we may need
+//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+    db: {
+        uri: 'mongodb+srv://testuser:testuser123@studyedge-as98i.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+    }
+};
